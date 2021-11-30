9609629ebc740e3848f8e37e7c1280cdba2cf480
Virendra Hingle <virendra.virchanderhingle@tesco.com>
2021-11-30 18:54:36 +0000

    deploy.sh script corrected

--- deployment/deploy-ppe.sh
+++ deployment/deploy-ppe.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kubectl config use-context euw-ppe-214-pma-aks-admin
-helm upgrade --install pma-frontend-service ./pma-frontend/ -f ./pma-frontend/values-ppe.yaml --set image.tag=$1 -n api
+helm secrets upgrade --install pma-frontend-service ./pma-frontend/ -f ./pma-frontend/values-ppe.yaml -f ./pma-frontend/secrets-ppe.yaml --set image.tag=$1 -n api
 
