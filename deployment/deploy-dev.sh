8452a8efd6af9e4d6dfdfd25ac727f4b03bdb6a9
Igor Tsentylo <igor.tsentilo@tesco.com>
2021-12-16 12:08:03 +0200

    ci: Format deployment scripts

--- deployment/deploy-dev.sh
+++ deployment/deploy-dev.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-kubectl config use-context euw-dev-214-pma-aks-admin
-helm secrets upgrade --install pma-frontend-service ./pma-frontend/ -f ./pma-frontend/values-dev.yaml -f ./pma-frontend/secrets-dev.yaml --set image.tag=$1 -n api
 
+kubectl config use-context euw-dev-214-pma-aks-admin
+
+helm secrets upgrade \
+    --install pma-frontend-service ./pma-frontend/ \
+    --set image.tag=$1 \
+    -f ./pma-frontend/values-dev.yaml \
+    -f ./pma-frontend/secrets-dev.yaml \
+    -n api
