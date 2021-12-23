bdfdf7a91f15c6032b2fec6a8aab7eb4f5010556
Vitalii <Vitalii.Kolodribskyi@tesco.com>
2021-12-23 13:11:42 +0200

    PMA-908: (client). Refactoring urls.

--- codebase/packages/client/src/pages/types.ts
+++ codebase/packages/client/src/pages/types.ts
@@ -2,8 +2,9 @@ import { ComponentType, FC, LazyExoticComponent } from 'react';
 
 enum Page {
   CONTRIBUTION = 'contribution',
-  CREATE_STRATEGIC_DRIVERS = 'create-strategic-drivers',
-  VIEW_STRATEGIC_DRIVERS = 'view-strategic-drivers',
+  CREATE_STRATEGIC_DRIVERS = 'strategic-drivers/create',
+  VIEW_STRATEGIC_DRIVER = 'strategic-drivers/:uuid',
+  VIEW_STRATEGIC_DRIVERS = 'strategic-drivers',
   CREATE_PERFORMANCE_CYCLE = 'performance-cycle/:performanceCycleUuid',
   PERFORMANCE_CYCLE = 'performance-cycle',
   PERSONAL_DEVELOPMENT_PLAN = 'personal-development-plan',
