db0614c6dad45c33f47713aeb759e8e4cccddec6
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2022-01-18 17:07:10 +0200

    fix(client): PMA-0000 add update feedback

--- codebase/packages/client/src/features/OuterGiveFeedBack/GiveNewFeedback.tsx
+++ codebase/packages/client/src/features/OuterGiveFeedBack/GiveNewFeedback.tsx
@@ -6,6 +6,40 @@ import { colleagueUUIDSelector, ColleaguesActions, FeedbackActions, feedbackByUu
 import { Icon } from 'components/Icon';
 import { Page } from 'pages';
 import { GiveFeedbackForm, ConfirmMassage, SuccessMassage, InfoMassage } from './components';
+import { GiveFeedbackType } from './type';
+
+const feedbackFields: GiveFeedbackType[] = [
+  {
+    id: '0',
+    code: 'Question 1',
+    title:
+      "Looking back at what you've seen recently, what would you like to say to this colleague about what they`ve delivered or how they've gone about it?",
+    description: "Share specific examples of what you've seen.",
+    field: {
+      id: '1',
+      type: 'textarea',
+    },
+  },
+  {
+    id: '1',
+    code: 'Question 2',
+    title: 'Looking forward, what should this colleague do more (or less) of in order to be at their best?',
+    description: 'Share your suggestions',
+    field: {
+      id: '2',
+      type: 'textarea',
+    },
+  },
+  {
+    id: '2',
+    code: 'Anything else?',
+    title: 'Add any other comments you would like to share with your colleague.',
+    field: {
+      id: '3',
+      type: 'textarea',
+    },
+  },
+];
 
 enum Statuses {
   PENDING = 'pending',
@@ -13,6 +47,7 @@ enum Statuses {
   SENDING = 'sending',
   INFO = 'info',
 }
+
 const GiveNewFeedback: FC = () => {
   const dispatch = useDispatch();
   const [status, setStatus] = useState(Statuses.PENDING);
@@ -21,7 +56,10 @@ const GiveNewFeedback: FC = () => {
     targetColleagueUuid: '',
     feedbackItems: [{ content: '' }, { content: '' }, { content: '' }],
   };
-  const [formData, setFormData] = useState({ feedbackItems, targetColleagueUuid });
+  const [formData, setFormData] = useState({
+    feedbackItems: feedbackFields.map(({ code }) => feedbackItems.find((item) => item.code === code)),
+    targetColleagueUuid,
+  });
   const colleagueUuid = useSelector(colleagueUUIDSelector);
   const navigate = useNavigate();
 
@@ -33,7 +71,21 @@ const GiveNewFeedback: FC = () => {
     setFormData(data);
 
     if (data.status === 'DRAFT') {
-      dispatch(FeedbackActions.createNewFeedback([{ ...data, colleagueUuid }]));
+      if (uuid === 'new') {
+        dispatch(FeedbackActions.createNewFeedback([{ ...data, colleagueUuid }]));
+      } else {
+        dispatch(
+          FeedbackActions.updatedFeedback({
+            ...data,
+            feedbackItems: data.feedbackItems.map((item) => ({
+              ...item,
+              uuid: feedbackItems.find((feedback) => feedback.code === item.code)?.uuid,
+            })),
+            colleagueUuid,
+            uuid,
+          }),
+        );
+      }
       handleSuccess();
       return;
     }
@@ -69,6 +121,7 @@ const GiveNewFeedback: FC = () => {
             setStatus(Statuses.INFO);
             setFormData(data);
           }}
+          feedbackFields={feedbackFields}
         />
       )}
       {status === Statuses.CONFIRMING && (
