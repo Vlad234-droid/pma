6a007c82d3e4359567a62a3f8e60e3ca37802fce
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2022-01-21 15:40:32 +0200

    fix(client): PMA-832 fix result status

---
+++ codebase/packages/client/src/features/MyTeam/utils/index.test.ts
@@ -0,0 +1,76 @@
+import { Timeline, ReviewType, Status, TimelineType } from 'config/types';
+import { generateEmployeeReview, generateTimelines, generateTimeline } from 'utils/test';
+
+import { getLastTimelineStatus } from './index';
+
+describe('<MyTeam /> utils', () => {
+  describe('#getLastTimelineStatus', () => {
+    it('should return Status.STARTED, if items has Status.STARTED', () => {
+      const timeline = generateTimelines(2, { status: Status.STARTED });
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline,
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.STARTED);
+    });
+
+    it('should return Status.STARTED, if all types are TIMELINE_POINT', () => {
+      const timeline = generateTimelines(2, { type: TimelineType.TIMELINE_POINT });
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline,
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.STARTED);
+    });
+
+    it('should return Status.STARTED, if there are no timeline with type different from TIMELINE_POINT or status different from STARTED', () => {
+      const timeline1 = generateTimeline({ type: TimelineType.TIMELINE_POINT });
+      const timeline2 = generateTimeline({ status: Status.STARTED });
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline: [timeline1, timeline2],
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.STARTED);
+    });
+
+    it('should return Status.PENDING, if last item in timelines has its status and is not TIMELINE_POINT', () => {
+      const timeline1 = generateTimeline({ type: TimelineType.TIMELINE_POINT });
+      const timeline2 = generateTimeline({ status: Status.STARTED });
+      const timeline3 = generateTimeline();
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline: [timeline1, timeline2, timeline3],
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.PENDING);
+    });
+
+    it('should return Status.PENDING, if last item without TIMELINE_POINT in timelines has its status', () => {
+      const timeline1 = generateTimeline({ type: TimelineType.TIMELINE_POINT });
+      const timeline2 = generateTimeline({ status: Status.STARTED });
+      const timeline3 = generateTimeline();
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline: [timeline1, timeline3, timeline2],
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.PENDING);
+    });
+
+    it('should return Status.Draft, if last item in timelines has its status and is not TIMELINE_POINT', () => {
+      const timeline1 = generateTimeline({ type: TimelineType.TIMELINE_POINT });
+      const timeline2 = generateTimeline({ status: Status.STARTED });
+      const timeline3 = generateTimeline();
+      const timeline4 = generateTimeline({ status: Status.DRAFT });
+      const employee = {
+        ...generateEmployeeReview(),
+        timeline: [timeline1, timeline3, timeline2, timeline4],
+      };
+
+      expect(getLastTimelineStatus(employee)).toBe(Status.DRAFT);
+    });
+  });
+});
--- codebase/packages/client/src/utils/test/generateEmployeeReview.ts
+++ codebase/packages/client/src/utils/test/generateEmployeeReview.ts
@@ -1,4 +1,4 @@
-import faker from 'faker';
+// import faker from 'faker';
 
 import { Employee } from 'config/types';
 
@@ -6,18 +6,29 @@ import { generateReviews } from './generateReview';
 import { generateTimelines } from './generateTimeline';
 
 const generateEmployeeReview = () => {
-  const review: Employee = {
-    businessType: faker.random.words(1),
-    firstName: faker.random.words(1),
-    jobName: faker.random.words(1),
-    lastName: faker.random.words(1),
-    middleName: faker.random.words(1),
-    uuid: faker.random.words(1),
+  const employee: Employee = {
+    businessType: 'mocked_business_type',
+    firstName: 'mocked_first_name',
+    jobName: 'mocked_job_name',
+    lastName: 'mocked_last_name',
+    middleName: 'mocked_middle_name',
+    uuid: 'mocked_uuid',
     reviews: generateReviews(2),
     timeline: generateTimelines(2),
   };
 
-  return review;
+  // const employee: Employee = {
+  //   businessType: faker.random.words(1),
+  //   firstName: faker.random.words(1),
+  //   jobName: faker.random.words(1),
+  //   lastName: faker.random.words(1),
+  //   middleName: faker.random.words(1),
+  //   uuid: faker.random.words(1),
+  //   reviews: generateReviews(2),
+  //   timeline: generateTimelines(2),
+  // };
+
+  return employee;
 };
 
 export { generateEmployeeReview };
