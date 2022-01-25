c5e24cc7fbe02c8f4683e2c8d5a18f946e8fdfd3
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2022-01-25 19:14:06 +0200

    feat(client): PMA-1044 Permission feature

---
+++ codebase/packages/client/src/features/Permission/constants/roles.ts
@@ -0,0 +1,6 @@
+export const TALENT_ADMIN = 'TalentAdmin';
+export const PROCESS_MANAGER = 'ProcessManager';
+export const PEOPLE_TEAM = 'PeopleTeam';
+export const COLLEAGUE = 'Colleague';
+export const LINE_MANAGER = 'LineManager';
+export const ADMIN = 'Admin';
---
+++ codebase/packages/client/src/features/Permission/hooks/usePermission.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import authContext from 'contexts/authContext';
+import { checkPermissions } from '../utils';
+
+export default (actions: Array<string>) => {
+  const { roles } = useContext(authContext);
+
+  return checkPermissions(actions, roles);
+};
