c75156328e0c65f545ae0e80493322b6346c4430
Roman Lysakov <roman.lysakov@tesco.com>
2022-01-26 12:58:18 +0200

    fix(client): PMA-000 pm-cycles templates upload (#297)
    * fix(client): PMA-0000 PMA|Integrate api for previous reviews
    
    * fix(client): PMA-0000 PMA|Integrate api for previous reviews
    - Step indicator gub fix
    
    * fix(client): PMA-000 added possibility to upload templates
    
    * fix(client): PMA-000 pm-cycles templates upload
    
    * fix(client): PMA-000 templates upload fix
    
    * fix(client): PMA-000 template upload fix
    
    Co-authored-by: Vitaliy Huz <vhuz@luxoft.com>

--- codebase/packages/api/src/rest/files.ts
+++ codebase/packages/api/src/rest/files.ts
@@ -2,7 +2,7 @@ import httpClient from '../config/client';
 
 const domain = '/files';
 export const getBPMNFiles = (params?: any) => {
-  return httpClient.get(`${domain}?type_in%5B0%5D=BPMN&includeFileContent=false`, { params: { ...params } });
+  return httpClient.get(`${domain}?type=1&status=2`, { params: { ...params } });
 };
 
 export const getProcessTemplateMetadata = (params?: any) => {
