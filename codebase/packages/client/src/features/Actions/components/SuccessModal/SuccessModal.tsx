a5c2136a6e9e919f59b2d9f4e0659cdbf6aae72f
Kateryna Kukunina <kukurina.ekaterina@tesco.com>
2022-01-28 18:19:57 +0200

    fix(client): PMA-1163 fix

--- codebase/packages/client/src/features/Actions/components/SuccessModal/SuccessModal.tsx
+++ codebase/packages/client/src/features/Actions/components/SuccessModal/SuccessModal.tsx
@@ -22,8 +22,8 @@ const Modal: FC<Props> = ({ review, status, onClose }) => {
       description={status === Status.DECLINED
         ? t('you_have_rejected', 'You’ve rejected this form as it doesn’t reflect the conversation you had with your colleague. ' +
         'Please pick up with them directly to discuss more.')
-        : `${t('you_have_approved', 'You have approved your colleague\'s')} ${review === ReviewType.OBJECTIVE ? t('objectives', 'Objectives')
-          : `${review} ${t('review', 'Review')}`}.`
+        : `${t('you_have_approved', 'You have approved your colleague\'s')} ${review === ReviewType.OBJECTIVE ? t('objectives', 'Objectives').toLowerCase()
+          : `${review === ReviewType.MYR ? t('review_type_description_myr', 'Mid-year review').toLowerCase() : t('review_type_description_eyr', 'Year-end review').toLowerCase()}`}.`
         }
     />
   );
