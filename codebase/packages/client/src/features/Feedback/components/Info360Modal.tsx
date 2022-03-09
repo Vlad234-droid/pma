96881cb3a4e0dc6521a03d38a4bf84a2a874b32b
Vitalii <Vitalii.Kolodribskyi@tesco.com>
2022-03-09 11:56:24 +0200

    feat(client): PMA-1439 CSS refactoring

--- codebase/packages/client/src/features/Feedback/components/Info360Modal.tsx
+++ codebase/packages/client/src/features/Feedback/components/Info360Modal.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { IconButton } from '../../../components/IconButton';
-import { useStyle, useBreakpoints, Rule, Styles, CreateRule, Theme } from '@dex-ddl/core';
+import { useStyle, useBreakpoints, Rule, Styles } from '@dex-ddl/core';
 import { Info360ModalProps } from '../config/types';
 import { VideoPlayer, VideoId } from 'features/VideoPlayer';
 import { Trans } from 'components/Translation';
@@ -8,18 +8,17 @@ import { Trans } from 'components/Translation';
 export const INFO_360_MODAL = 'info_360_modal';
 
 const Info360Modal: FC<Info360ModalProps> = ({ setInfo360Modal }) => {
-  const { css, theme } = useStyle();
-  const [, isBreakpoint] = useBreakpoints();
-  const mobileScreen = isBreakpoint.small || isBreakpoint.xSmall;
+  const { css } = useStyle();
+
   return (
     <div className={css(WrapperInfo)} data-test-id={INFO_360_MODAL}>
       <div className={css({ height: '100%' })}>
-        <div className={css(title({ theme }))}>
+        <div className={css(title)}>
           <Trans i18nKey='difference_between_everyday_feedback_and_feedback_360'>
             What is the difference between ‘Everyday feedback’ and ‘360 feedback’?
           </Trans>
         </div>
-        <p className={css(PreTitle({ theme }))}>
+        <p className={css(PreTitle)}>
           <Trans i18nKey='you_can_give_and_receive_feedback_with_your_colleagues'>
             You can give and receive feedback with your colleagues using Everyday feedback or 360 feedback.
           </Trans>
@@ -50,7 +49,7 @@ const Info360Modal: FC<Info360ModalProps> = ({ setInfo360Modal }) => {
           </Trans>
         </p>
 
-        <h2 className={css(TitleVideo({ theme }))}>
+        <h2 className={css(TitleVideo)}>
           <Trans i18nKey='watch_this_video_on_the_importance_of_feedback'>
             Watch this 2-minute video on the importance of feedback:
           </Trans>
@@ -58,20 +57,20 @@ const Info360Modal: FC<Info360ModalProps> = ({ setInfo360Modal }) => {
         <div className={css(BlockVideoExplanation)}>
           <VideoPlayer videoId={VideoId.LEARN_FEEDBACK} />
         </div>
-        <h3 className={css(RecomendationInfo({ theme }))}>
+        <h3 className={css(RecomendationInfo)}>
           <Trans i18nKey='face_to_face_conversations'>Face to face conversations are best for giving feedback.</Trans>
         </h3>
-        <p className={css(PreRecomendationInfo({ theme }))}>
+        <p className={css(PreRecomendationInfo)}>
           <Trans i18nKey='sharing_feedback_face_to_face_gives'>
             Sharing feedback face to face gives you the space to have a two-way conversation, building trust and working
             together on what the colleague could do next. Where this isn&apos;t possible, you can use the feedback
             function in the Your Contribution system.
           </Trans>
         </p>
-        <h3 className={css(RecomendationInfo({ theme }))}>
+        <h3 className={css(RecomendationInfo)}>
           <Trans i18nKey='want_to_say_thank_you_to_a_colleague'>Want to say “thank you” to a colleague?</Trans>
         </h3>
-        <p className={css(PreRecomendationInfo({ theme }))}>
+        <p className={css(PreRecomendationInfo)}>
           <Trans i18nKey='recognition_is_important_as_all_other_feedback'>
             Recognition is just as important as all other feedback. We need to remember to celebrate our successes,
             share praise or say &apos;thank you&apos; to our colleagues when things have gone well. You can share
@@ -80,16 +79,7 @@ const Info360Modal: FC<Info360ModalProps> = ({ setInfo360Modal }) => {
           </Trans>
         </p>
       </div>
-      <span
-        className={css({
-          position: 'fixed',
-          top: theme.spacing.s5,
-          left: mobileScreen ? theme.spacing.s5 : theme.spacing.s10,
-          textDecoration: 'none',
-          border: 'none',
-          cursor: 'pointer',
-        })}
-      >
+      <span className={css(arrowSpan)}>
         <IconButton
           graphic='arrowLeft'
           onPress={() => setInfo360Modal(() => false)}
@@ -100,7 +90,21 @@ const Info360Modal: FC<Info360ModalProps> = ({ setInfo360Modal }) => {
   );
 };
 
-const title: CreateRule<{ theme: Theme }> = ({ theme }) => ({
+const arrowSpan: Rule = ({ theme }) => {
+  const [, isBreakpoint] = useBreakpoints();
+  const mobileScreen = isBreakpoint.small || isBreakpoint.xSmall;
+
+  return {
+    position: 'fixed',
+    top: theme.spacing.s5,
+    left: mobileScreen ? theme.spacing.s5 : `${theme.spacing.s10}`,
+    textDecoration: 'none',
+    border: 'none',
+    cursor: 'pointer',
+  };
+};
+
+const title: Rule = ({ theme }) => ({
   fontSize: `${theme.font.fixed.f20.fontSize}`,
   fontWeight: `${theme.font.weight.bold}`,
   marginBottom: '20px',
@@ -112,14 +116,14 @@ const WrapperInfo: Rule = {
   height: '100%',
 };
 
-const PreTitle: CreateRule<{ theme: Theme }> = ({ theme }) => ({
+const PreTitle: Rule = ({ theme }) => ({
   margin: '16px 0px 0px 0px',
   fontWeight: 'normal',
   fontSize: `${theme.font.fixed.f16.fontSize}`,
   lineHeight: '20px',
 });
 
-const TitleVideo: CreateRule<{ theme: Theme }> = ({ theme }) => ({
+const TitleVideo: Rule = ({ theme }) => ({
   fontWeight: `${theme.font.weight.bold}`,
   fontSize: `${theme.font.fixed.f20.fontSize}`,
   lineHeight: `${theme.font.fixed.f24.lineHeight}`,
@@ -136,14 +140,14 @@ const BlockVideoExplanation: Rule = {
   },
 } as Styles;
 
-const RecomendationInfo: CreateRule<{ theme: Theme }> = ({ theme }) => ({
+const RecomendationInfo: Rule = ({ theme }) => ({
   fontWeight: `${theme.font.weight.bold}`,
   fontSize: `${theme.font.fixed.f20.fontSize}`,
   lineHeight: `${theme.font.fixed.f20.lineHeight}`,
   marginBottom: '16px',
 });
 
-const PreRecomendationInfo: CreateRule<{ theme: Theme }> = ({ theme }) => ({
+const PreRecomendationInfo: Rule = ({ theme }) => ({
   fontWeight: `${theme.font.weight.light}`,
   fontSize: `${theme.font.fixed.f16.fontSize}`,
   lineHeight: `${theme.font.fixed.f20.lineHeight}`,
