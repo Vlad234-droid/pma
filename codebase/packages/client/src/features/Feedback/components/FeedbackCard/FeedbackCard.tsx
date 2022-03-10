4c51c409404104722f19b39816268b784fb2eb7e
Vitalii <Vitalii.Kolodribskyi@tesco.com>
2022-03-10 16:17:47 +0200

    feat(client): PMA-1511 PDF Export, fix link on Feedback page

--- codebase/packages/client/src/features/Feedback/components/FeedbackCard/FeedbackCard.tsx
+++ codebase/packages/client/src/features/Feedback/components/FeedbackCard/FeedbackCard.tsx
@@ -16,7 +16,7 @@ const FeedbackCard: FC<FeedbackCardProps> = ({ card }) => {
 
   return (
     <div className={css(cardStyle)} data-test-id={FEEDBACK_CARD_WRAPPER}>
-      <Link to={buildPath(card.link)}>
+      <Link to={card.link}>
         <TileWrapper>
           <div className={css(wrapperBlock)}>
             <div className={css(actionStyle)}>{card.action}</div>
