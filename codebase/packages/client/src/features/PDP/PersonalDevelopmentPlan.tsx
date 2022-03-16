debbddb3b7c698bc62a62fd6fa4e8fc4ddb5205b
Vitalii <Vitalii.Kolodribskyi@tesco.com>
2022-03-16 16:37:42 +0200

    feat(client): PMA-1548 UI fixes

--- codebase/packages/client/src/features/PDP/PersonalDevelopmentPlan.tsx
+++ codebase/packages/client/src/features/PDP/PersonalDevelopmentPlan.tsx
@@ -91,7 +91,7 @@ const PersonalDevelopmentPlan: FC = () => {
   const downloadHref = () => `${BASE_URL_API}/pdp/template`;
 
   return (
-    <div className={css({ padding: '0 40px' })}>
+    <div className={css(wrapper({ mobileScreen }))}>
       <div className={css(buttonBlock)}>
         <div className={css(controlButtons({ mobileScreen }))}>
           {pdpSelector?.length !== 5 && (
@@ -209,6 +209,20 @@ const PersonalDevelopmentPlan: FC = () => {
   );
 };
 
+const wrapper: CreateRule<{ mobileScreen: boolean }> = (props) => {
+  const { mobileScreen } = props;
+
+  if (mobileScreen) {
+    return {
+      padding: '0 10px',
+    };
+  }
+
+  return {
+    padding: '0 40px',
+  };
+};
+
 const subtitleBlock = {
   display: 'flex',
   justifyContent: 'space-between',
@@ -247,7 +261,7 @@ const descriptionMain = {
   justifyContent: 'space-between',
   alignItems: 'flex-start',
   flexDirection: 'row',
-  width: '70%',
+  width: '100%',
   '@media (max-width: 900px)': {
     flexDirection: 'column',
   },
