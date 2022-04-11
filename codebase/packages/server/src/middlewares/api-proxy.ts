08c7a230a33422d6827c91e9da5bd7a66a0c063a
Igor Tsentylo <tsentylo@gmail.com>
2022-04-11 10:30:48 +0300

    feat(ci)/PMA-000: Rework BFF proxy for API and add one for CAMUNDA

---
+++ codebase/packages/server/src/middlewares/api-proxy.ts
@@ -0,0 +1,41 @@
+import { Response } from 'express';
+import { createProxyMiddleware, Options } from 'http-proxy-middleware';
+import { getIdentityData } from '@pma-connectors/onelogin';
+
+import { isDEV, isLocal, ProcessConfig } from "../config";
+
+export const apiProxyMiddleware = (config: ProcessConfig) => {
+
+  const proxyMiddlewareOptions: Options = {
+    target: config.proxyApiServerUrl(),
+    changeOrigin: true,
+    autoRewrite: true,
+    pathRewrite: { ['^/api']: '' },
+    logLevel: 'debug',
+  };
+
+  proxyMiddlewareOptions.onError = (e) => {
+    console.log('e', e);
+  };
+  
+  if (!isDEV(config.buildEnvironment()) && config.useOneLogin()) {
+    proxyMiddlewareOptions.onProxyReq = function (proxyReq, req, res) {
+      const identityData = getIdentityData(res as Response);
+
+      console.log('[HPM] Clear all cookies');
+      proxyReq.setHeader('Cookie', '');
+
+      proxyReq.setHeader('Authorization', `Bearer ${identityData?.access_token}`);
+
+      if (isLocal(config.buildEnvironment())) {
+        console.log('[HPM] Authorization: bearer-jwt-identity', identityData?.access_token);
+      }
+    };
+  }
+
+  return createProxyMiddleware(
+    ['/api/**', '!/api/v1/camunda/**', '!/api/colleague-inbox/**', '!/api/manager-bff/**'],
+    proxyMiddlewareOptions,
+  );
+
+}
--- codebase/packages/server/src/middlewares/index.ts
+++ codebase/packages/server/src/middlewares/index.ts
@@ -1,5 +1,7 @@
 export * from './auth';
 // export * from './open-id';
+export * from './api-proxy';
+export * from './camunda-proxy';
 export * from './open-id-new';
 export * from './asset-handler';
 export * from './error-handler';
