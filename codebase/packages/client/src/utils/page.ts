715f5f6b5c2bfadf360d5fa5b420183504387871
Ruslan Sapizhiuk <ruslan.sapizhiuk@tesco.com>
2022-04-18 10:39:44 +0300

    Bugfix/camunda (#708)
    * fix(client): PMA-0000 get PUBLIC_URL from config
    
    * fix(client): PMA-0000 check url
    
    * feat(client): PMA-0000 addIfNeededSlash fn

--- codebase/packages/client/src/utils/page.ts
+++ codebase/packages/client/src/utils/page.ts
@@ -1,4 +1,8 @@
-const PUBLIC_URL = process.env.PUBLIC_URL || '/';
+import { PUBLIC_URL } from 'config/constants';
+
+const URL_REGEXP = /\/$/;
+
+const addIfNeededSlash = (path: string) => (URL_REGEXP.test(path) ? `${path}/` : path);
 
 const paramsReplacer = (template: string, replace: Record<string, string>): string =>
   template
@@ -6,6 +10,6 @@ const paramsReplacer = (template: string, replace: Record<string, string>): stri
     .map((path) => path.replace(/(:.*)/, (_, $2) => `${replace[$2]}`))
     .join('/');
 
-const buildAbsolutePath = (path: string) => `${PUBLIC_URL}${path}`;
+const buildAbsolutePath = (path: string) => `${addIfNeededSlash(PUBLIC_URL)}${path}`;
 
 export { paramsReplacer, buildAbsolutePath };
