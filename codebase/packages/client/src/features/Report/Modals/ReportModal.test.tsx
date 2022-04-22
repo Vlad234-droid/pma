a72e845d7d94e7bde6ce6d7a6d21a09984079b30
Ruslan Sapizhiuk <ruslan.sapizhiuk@tesco.com>
2022-04-22 16:51:08 +0300

    fix(client): PMA-000 fix modal test (#740)

--- codebase/packages/client/src/features/Report/Modals/ReportModal.test.tsx
+++ codebase/packages/client/src/features/Report/Modals/ReportModal.test.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import { renderWithTheme as render } from 'utils/test';
-import DownloadReportModal, { DOWNLOAD_WRAPPER } from './ReportModal';
+import ReportModal, { DOWNLOAD_WRAPPER } from './ReportModal';
 
 describe('Download report modal', () => {
   const onClose = jest.fn();
   const props = {
     onClose,
+    modalStatus: null,
   };
   it('it should render download wrapper', async () => {
-    const { getByTestId } = render(<DownloadReportModal {...props} />);
+    const { getByTestId } = render(<ReportModal {...props} />);
 
     const wrapper = getByTestId(DOWNLOAD_WRAPPER);
     expect(wrapper).toBeInTheDocument();
   });
   it('it call onClose handler', async () => {
-    const { getByText } = render(<DownloadReportModal {...props} />);
+    const { getByText } = render(<ReportModal {...props} />);
     const cancel = getByText('Cancel');
     expect(cancel).toBeInTheDocument();
   });
