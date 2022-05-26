d4e6471a7b8dc43244743c7334cca06aea3a2d31
Vitalii Huz <vitalii.huz@tesco.com>
2022-05-26 16:49:26 +0300

    fix(client): PMA-1631 Notification switches are not displayed  (#815)
    * fix(client): PMA-1631 Notification switches are not displayed (PROD, PPE, DEV)
    
    * fix(client): PMA-1631 Notification switches are not displayed (PROD, PPE, DEV)
    - add tap
    
    Co-authored-by: vitaliy-guz <vitaliy.guz@gmail.com>

--- codebase/packages/store/src/entities/user/epic.ts
+++ codebase/packages/store/src/entities/user/epic.ts
@@ -2,18 +2,29 @@
 import { Epic, isActionOf } from 'typesafe-actions';
 import { combineEpics } from 'redux-observable';
 import { from, of } from 'rxjs';
-import { catchError, filter, map, switchMap, takeUntil, mergeMap } from 'rxjs/operators';
+import { catchError, filter, map, mergeMap, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { concatWithErrorToast, errorPayloadConverter } from '../../utils/toastHelper';
 
-import { getCurrentUser, updateUserNotification, createProfileAttribute, updateProfileAttribute } from './actions';
+import { createProfileAttribute, getCurrentUser, updateProfileAttribute, updateUserNotification } from './actions';
+
+const NUMBER_OF_DEFAULT_ATTRIBUTES = 18;
 
 export const getCurrentUserEpic: Epic = (action$, _, { openapi }) =>
   action$.pipe(
     filter(isActionOf(getCurrentUser.request)),
     switchMap(() =>
       from(openapi.user.getMe()).pipe(
-        // @ts-ignore
-        map(getCurrentUser.success),
+        tap((data) => {
+          // @ts-ignore
+          const profileAttributes = data.data?.profileAttributes;
+          if (!profileAttributes || profileAttributes.length < NUMBER_OF_DEFAULT_ATTRIBUTES) {
+            // @ts-ignore
+            openapi.config.updateDefaultAttributes({ colleagueUuid: data.data?.colleague.colleagueUUID });
+          }
+        }),
+        map((data) => {
+          return getCurrentUser.success(data as any);
+        }),
         catchError((e) => {
           const { status, data } = e || {};
           if (status >= 500) {
