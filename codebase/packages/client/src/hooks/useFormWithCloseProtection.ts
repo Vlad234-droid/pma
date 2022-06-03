f52ad5c94d9317d3679b0dfcb68bcbc7071b1b90
Wojciech Kudyniuk <wojciech.kudyniuk@tesco.com>
2022-06-03 12:48:13 +0200

    fix/PMA-726-message-for-unsaved-fields (#842)

--- codebase/packages/client/src/hooks/useFormWithCloseProtection.ts
+++ codebase/packages/client/src/hooks/useFormWithCloseProtection.ts
@@ -4,9 +4,11 @@ import { useForm as useReactHookForm } from 'react-hook-form';
 export const useFormWithCloseProtection: typeof useReactHookForm = (props) => {
   const form = useReactHookForm(props);
   const values = form.getValues();
-  const isMyDirty = !props?.defaultValues && Object.values(values).filter((el) => el).length;
-  const isDirty = form.formState.isDirty || isMyDirty;
-
+  /* 
+    TODO: Fix all forms - register field correctly and use defaultValues. form.formState.isDirty isn't working correctly, it depends on defaultValues and field register, but not all forms have it correctly setuped. 
+  */
+  const isDirty = Object.values(values).filter((el) => Array.isArray(el) ? el.length : el).length;
+  
   useEffect(() => {
     if (isDirty) {
       window.onbeforeunload = () => true;
