c8b1dbdc52091dfab46f55ede8542d9764d2643f
Igor Tsentylo <tsentylo@gmail.com>
2022-06-03 11:51:25 +0300

    fix/no-jira: Minor refactoring

--- codebase/packages/api/src/config/client.ts
+++ codebase/packages/api/src/config/client.ts
@@ -2,6 +2,4 @@ import { createHttpClient } from '../utils';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const baseURL = `${API_URL}`;
-
-export default createHttpClient(baseURL);
+export default createHttpClient(API_URL);
