10e90fa7cb6bd226459ce7cfa63a69aeca256826
Igor Tsentylo <tsentylo@gmail.com>
2022-06-09 12:42:13 +0300

    fix(server)/no-jira: fix swagger proxy (4)

--- codebase/packages/server/src/middlewares/swagger-proxy.ts
+++ codebase/packages/server/src/middlewares/swagger-proxy.ts
@@ -14,7 +14,7 @@ import { initializeProxyMiddleware } from './proxy';
 
 export const swaggerProxyMiddleware = (processConfig: ProcessConfig) => { 
   
-  const { applicationContextPath,  swaggerServerUrl, loggerLevel }= processConfig;
+  const { applicationContextPath, applicationUrl, swaggerServerUrl, loggerLevel }= processConfig;
 
   const swaggerProxyLogger = createLogger({ name: 'swagger' });
   const appRouter = Router();
@@ -24,6 +24,58 @@ export const swaggerProxyMiddleware = (processConfig: ProcessConfig) => {
       res.redirect(`${emptyIfRoot(applicationContextPath())}/swagger-ui/index.html`);
     }
   });
+
+  appRouter.get('/swagger-ui/swagger-initializer.js', (req, res) => {
+    const swaggerInitJsBuffer = Buffer.from(`
+      window.onload = function() {
+        // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+        window.ui = SwaggerUIBundle({
+          url: '',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+          ],
+          plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+          ],
+          layout: 'StandaloneLayout',
+          configUrl: '${emptyIfRoot(applicationContextPath())}/api-docs/swagger-config',
+          displayRequestDuration: true,
+          operationsSorter: 'alpha',
+          tagsSorter: 'alpha',
+          'urls.primaryName': 'pma-api-v1',
+          validatorUrl: ''
+        });
+      };
+    `);
+
+    res.contentType('application/javascript');
+    res.send(swaggerInitJsBuffer);
+  });
+
+  appRouter.get('/api-docs/swagger-config', (req, res) => {
+    const swaggerConfig = {
+      configUrl:`${emptyIfRoot(applicationContextPath())}/api-docs/swagger-config`,
+      displayRequestDuration:true,
+      oauth2RedirectUrl: `${applicationUrl().toString()}/swagger-ui/oauth2-redirect.html`,
+      operationsSorter: 'alpha',
+      tagsSorter: 'alpha',
+      urls:[
+        { url: `${emptyIfRoot(applicationContextPath())}/api-docs/pma-api-v1`, name: 'pma-api-v1' },
+        { url: `${emptyIfRoot(applicationContextPath())}/api-docs/user-management-v1`, name: 'user-management-v1' },
+        { url: `${emptyIfRoot(applicationContextPath())}/api-docs/x-actuator`, name: 'x-actuator' },
+      ],
+      'urls.primaryName': 'pma-api-v1',
+      validatorUrl: '',
+    };
+
+    const swaggerConfigBuffer = Buffer.from(JSON.stringify(swaggerConfig));
+
+    res.contentType('application/json');
+    res.send(swaggerConfigBuffer);
+  })
 
   appRouter.use(initializeProxyMiddleware({
     //filter: [ '/swagger-ui.html', '/swagger-ui', '/api-docs' ], 
