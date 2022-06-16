2a3d63a5efd6955d96f07086332a0171ee2332d5
Igor Tsentylo <tsentylo@gmail.com>
2022-06-16 09:42:20 +0300

    feat(ci)/PMA-2071: Pass CI/CD vars to runtime

--- deployment/deploy-dev.sh
+++ deployment/deploy-dev.sh
@@ -5,6 +5,16 @@ kubectl config use-context euw-dev-214-pma-aks-admin
 helm secrets upgrade \
     --install pma-frontend-service ./pma-frontend/ \
     --set image.tag=$1 \
+    --set azdevops.build.buildId=${BUILD_BUILDID} \
+    --set azdevops.build.buildNumber=${BUILD_BUILDNUMBER} \
+    --set azdevops.build.buildDefinitionName=${BUILD_DEFINITIONNAME} \
+    --set azdevops.build.sourceBranchName=${BUILD_SOURCEBRANCHNAME} \
+    --set azdevops.build.sourceBranch=${BUILD_SOURCEBRANCH} \
+    --set azdevops.build.sourceVersion=${BUILD_SOURCEVERSION} \
+    --set azdevops.release.releaseId=${RELEASE_RELEASEID} \
+    --set azdevops.release.releaseName=${RELEASE_RELEASENAME} \
+    --set azdevops.release.releaseDefinitionName=${RELEASE_DEFINITIONNAME} \
+    --set azdevops.release.environmentName=${RELEASE_ENVIRONMENTNAME} \
     -f ./pma-frontend/values-dev.yaml \
     -f ./pma-frontend/secrets-dev.yaml \
     -n api
