d82b4e64059c365e4c091de72268ba4c463b84fc
Vladyslav Baryshpolets <vladyslav.baryshpolets@tesco.com>
2022-10-03 10:57:28 +0300

    feat(client): PMA-000 add quarterly priorities to menu (#1156)
    Co-authored-by: Vlad Baryshpolets <vladikvladvita@gmail.com>

--- codebase/packages/client/src/burger-menu/general/quarterly-priorities/components/QuarterlyPriorities.tsx
+++ codebase/packages/client/src/burger-menu/general/quarterly-priorities/components/QuarterlyPriorities.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { colleagueUUIDSelector, timelinesExistSelector } from '@pma/store';
+import { useSelector } from 'react-redux';
+
+import { MenuItem as Item } from 'components/MenuItem';
+import { buildPath } from 'features/general/Routes';
+import { useTranslation } from 'components/Translation';
+import { Page } from 'pages';
 
 const MenuItem = () => {
-  return null;
-  //TODO: enabled when page will be developed
-  // return <Item iconGraphic={'goal'} title={t('quarterly_priorities', 'Quarterly Priorities')} />;
+  const { t } = useTranslation();
+  const colleagueUuid = useSelector(colleagueUUIDSelector);
+  const timelineExist = useSelector(timelinesExistSelector(colleagueUuid));
+
+  if (!timelineExist) return null;
+
+  return (
+    <Item
+      iconGraphic={'goal'}
+      linkTo={buildPath(Page.REVIEWS_VIEW)}
+      title={t('quarterly_priorities', 'Quarterly Priorities')}
+    />
+  );
 };
 
 export default MenuItem;
