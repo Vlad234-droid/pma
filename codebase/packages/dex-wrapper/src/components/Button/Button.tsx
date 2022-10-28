d2d80c54386af0cc750d2df6cc820fb75ff64c5a
Serhii Yakushov <serhii.yakushov@tesco.com>
2022-10-28 10:40:39 +0300

    fix(client): PMA-2816 (#1219)

--- codebase/packages/dex-wrapper/src/components/Button/Button.tsx
+++ codebase/packages/dex-wrapper/src/components/Button/Button.tsx
@@ -46,7 +46,6 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = memo((props) => {
           alignItems: 'center',
           lineHeight: '0',
           outline: 0,
-          opacity: 1,
         },
         ...styles,
       )}
