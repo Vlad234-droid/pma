2797ff3bbeb7e410d214d8767e27315152492ea7
Ruslan Sapizhuk <ruslan.sapizhuk@gmail.com>
2022-11-09 15:01:34 +0200

    fix(api): PMA-2630 update getManagersReviews

--- codebase/packages/api/src/rest/managers.ts
+++ codebase/packages/api/src/rest/managers.ts
@@ -5,19 +5,10 @@ const domain = '/managers';
 const REVIEWS = 'reviews';
 const FULL_TEAM_REVIEWS = 'reviews/full-team';
 
-export const getManagersReviews = (params: {
-  colleagueUuid: string;
-  fullTeam: string;
-  colleagueCycleStatuses?: string[];
-  ({ ...any });
-}) => {
-  const { colleagueUuid, fullTeam, colleagueCycleStatuses, ...restParams } = params;
-
-  const parameters = colleagueCycleStatuses
-    ? { colleagueCycleStatuses: colleagueCycleStatuses.toString(), ...restParams }
-    : restParams;
+export const getManagersReviews = (params: { colleagueUuid: string; fullTeam: string; ({ ...any }) }) => {
+  const { colleagueUuid, fullTeam, ...restParams } = params;
 
   return httpClient.get(`${domain}/${colleagueUuid}/${fullTeam ? FULL_TEAM_REVIEWS : REVIEWS}`, {
-    params: parameters,
+    params: restParams,
   });
 };
