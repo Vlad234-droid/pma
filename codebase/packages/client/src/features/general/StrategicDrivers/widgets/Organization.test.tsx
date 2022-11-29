17c019edf4202526a9003c5cb044de72faf838ee
Maciej Gołas <maciej.golas1@tesco.com>
2022-11-29 18:17:47 +0100

    fix(client): PMA-2997 fix organization tests (#1344)

--- codebase/packages/client/src/features/general/StrategicDrivers/widgets/Organization.test.tsx
+++ codebase/packages/client/src/features/general/StrategicDrivers/widgets/Organization.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as reactRedux from 'react-redux';
 import '@testing-library/jest-dom/extend-expect';
 import { renderWithTheme as render } from 'utils/test';
 import { fireEvent } from '@testing-library/react';
@@ -15,36 +16,56 @@ describe('Organization widget', () => {
     description: 'Your organization has 6 drivers share to all colleagues',
     actionTitle: 'View',
   };
-  it('it should render Organization widget', async () => {
-    const { getByTestId } = render(<Organization {...props} />);
-    const wrapper = getByTestId(TILE_WRAPPER);
 
-    expect(wrapper).toBeInTheDocument();
+  describe('Objectives shared', () => {
+    beforeEach(() => {
+      jest.spyOn(reactRedux, 'useSelector').mockReturnValue(['test']);
+    });
+
+    it('it should render Organization widget', async () => {
+      const { getByTestId } = render(<Organization {...props} />);
+      const wrapper = getByTestId(TILE_WRAPPER);
+
+      expect(wrapper).toBeInTheDocument();
+    });
+    it('it should render Organization content', async () => {
+      const { getByText } = render(<Organization {...props} />);
+      const description = getByText(content.description);
+      const actionTitle = getByText(content.actionTitle);
+
+      expect(description).toBeInTheDocument();
+      expect(actionTitle).toBeInTheDocument();
+    });
+
+    it('it should render Organization content', async () => {
+      const { getByText } = render(<Organization {...props} />);
+      const description = getByText(content.description);
+      const actionTitle = getByText(content.actionTitle);
+
+      expect(description).toBeInTheDocument();
+      expect(actionTitle).toBeInTheDocument();
+    });
+
+    it('it should call onClick', async () => {
+      const { getByText } = render(<Organization {...props} />);
+      const button = getByText(content.actionTitle);
+
+      fireEvent.click(button);
+
+      expect(onClick).toHaveBeenCalledTimes(1);
+    });
   });
-  it('it should render Organization content', async () => {
-    const { getByText } = render(<Organization {...props} />);
-    const description = getByText(content.description);
-    const actionTitle = getByText(content.actionTitle);
 
-    expect(description).toBeInTheDocument();
-    expect(actionTitle).toBeInTheDocument();
-  });
+  describe('No objectives shared', () => {
+    beforeEach(() => {
+      jest.spyOn(reactRedux, 'useSelector').mockReturnValue(undefined);
+    });
 
-  it('it should render Organization content', async () => {
-    const { getByText } = render(<Organization {...props} />);
-    const description = getByText(content.description);
-    const actionTitle = getByText(content.actionTitle);
+    it('it should not render Organization widget', () => {
+      const { queryByTestId } = render(<Organization {...props} />);
+      const wrapper = queryByTestId(TILE_WRAPPER);
 
-    expect(description).toBeInTheDocument();
-    expect(actionTitle).toBeInTheDocument();
-  });
-
-  it('it should call onClick', async () => {
-    const { getByText } = render(<Organization {...props} />);
-    const button = getByText(content.actionTitle);
-
-    fireEvent.click(button);
-
-    expect(onClick).toHaveBeenCalledTimes(1);
+      expect(wrapper).not.toBeInTheDocument();
+    });
   });
 });
