8a8a1cad16f639dd05ae0d8283cd075af82ef78d
Wojciech Kudyniuk <wojciech.kudyniuk@tesco.com>
2022-11-30 13:15:10 +0100

    feat: PMA-2954 splunk logs

--- codebase/packages/client/src/features/general/Permission/utils/checkPermissions.ts
+++ codebase/packages/client/src/features/general/Permission/utils/checkPermissions.ts
@@ -1,19 +1,7 @@
-import { uiLogger } from 'error-reporting';
-import { LogLevel } from '@energon/splunk-logger-core';
-
 const checkPermissions = (performs: Array<string>, actions: Array<string>) => {
   if (!performs.length) return false;
 
-  const hasAccess = performs.some((perform) => actions.includes(perform));
-
-  if (!hasAccess) {
-    uiLogger(LogLevel.warn, 'Access denied', {
-      has: actions,
-      required: performs,
-    });
-  }
-
-  return hasAccess;
+  return performs.some((perform) => actions.includes(perform));
 };
 
 export default checkPermissions;
