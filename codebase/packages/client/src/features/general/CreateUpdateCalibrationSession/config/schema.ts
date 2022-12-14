1da00f8992995e2c901e76823e5371f2dfc050b5
Serhii Yakushov <serhii.yakushov@tesco.com>
2022-12-14 10:31:07 +0200

    fix(client): PMA-3053 option 4 (#1388)

--- codebase/packages/client/src/features/general/CreateUpdateCalibrationSession/config/schema.ts
+++ codebase/packages/client/src/features/general/CreateUpdateCalibrationSession/config/schema.ts
@@ -4,14 +4,11 @@ import { TFunction } from 'components/Translation';
 export const createSchema = (t: TFunction) =>
   Yup.object().shape({
     title: Yup.string()
-      .max(
-        100,
-        t('field_must_be_less_than_number_characters', `Field must be less than 100 characters`, { value: 100 }),
-      )
+      .max(100, t('must_be_at_most_number_characters', `Must be at most 100 characters`, { value: 100 }))
       .required(t('field_is_required')),
     startTime: Yup.string().required(t('field_is_required')),
     description: Yup.string().max(
       100,
-      t('field_must_be_less_than_number_characters', `Field must be less than 100 characters`, { value: 100 }),
+      t('must_be_at_most_number_characters', `Must be at most 100 characters`, { value: 100 }),
     ),
   });
