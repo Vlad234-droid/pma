c139455e67c69f41cf41a2a06751b54becf2bc4c
paweljurczynski <pawel.jurczynski@tesco.com>
2023-03-03 07:26:53 +0100

    fix(client): PMA-3682 – Change "Save and Exit" to "Close" on approved priority

--- codebase/packages/client/src/features/bank/CreateUpdateObjectives/components/ButtonsModifySingleStep/ButtonsModifySingleStep.tsx
+++ codebase/packages/client/src/features/bank/CreateUpdateObjectives/components/ButtonsModifySingleStep/ButtonsModifySingleStep.tsx
@@ -1,24 +1,41 @@
 import React, { FC } from 'react';
 import { Button, CreateRule, Rule, useStyle } from '@pma/dex-wrapper';
 import { Trans } from 'components/Translation';
+import { Status } from '../../../../../config/enum';
 
 type ButtonsProps = {
   onSaveAndExit: () => void;
   isValid: boolean;
   onSubmit: (T?) => void;
+  status?: Status;
+  onClose: () => void;
 };
 
-const Buttons: FC<ButtonsProps> = ({ onSubmit, onSaveAndExit, isValid }) => {
+const Buttons: FC<ButtonsProps> = ({ onSubmit, onSaveAndExit, isValid, status, onClose }) => {
   const { css, matchMedia } = useStyle();
   const mobileScreen = matchMedia({ xSmall: true, small: true }) || false;
+
+  const renderExitButton = () => {
+    if (status === Status.APPROVED) {
+      return (
+        <Button onPress={onClose} styles={[buttonWhiteStyle]}>
+          <Trans i18nKey='close'>Close</Trans>
+        </Button>
+      );
+    }
+
+    return (
+      <Button onPress={onSaveAndExit} styles={[buttonWhiteStyle]}>
+        <Trans i18nKey='save_and_exit'>Save and exit</Trans>
+      </Button>
+    );
+  };
 
   return (
     <div className={css(containerStyle)}>
       <div className={css(wrapperStyle)}>
         <div className={css(buttonWrapperStyle({ mobileScreen }))}>
-          <Button onPress={onSaveAndExit} styles={[buttonWhiteStyle]}>
-            <Trans i18nKey='save_and_exit'>Save and exit</Trans>
-          </Button>
+          {renderExitButton()}
           <Button onPress={onSubmit} styles={[buttonBlueStyle]} isDisabled={!isValid}>
             <Trans i18nKey='submit'>Submit</Trans>
           </Button>
