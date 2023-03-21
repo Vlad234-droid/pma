6443c581733bf2f35cc40b7b4d173951496b4987
Kacper Jach <kacper.jach@tesco.com>
2023-03-21 18:55:23 +0100

    fix: PMA-3116 failing test fix (#1828)

--- codebase/packages/client/jest.setup.js
+++ codebase/packages/client/jest.setup.js
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import { configure } from '@testing-library/react';
 import { jest } from '@jest/globals';
+import { TextEncoder, TextDecoder } from 'util';
 
 global.matchMedia =
   global.matchMedia ||
@@ -12,5 +13,8 @@ global.matchMedia =
       removeListener: jest.fn(),
     };
   };
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 configure({ testIdAttribute: 'data-test-id' });
