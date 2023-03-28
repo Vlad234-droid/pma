128a2d2851b8d66d96f449a1f900884f3a34c3aa
Kacper Jach <kacper.jach@tesco.com>
2023-03-28 12:49:24 +0200

    fix: PMA-3116  changing config for azure (#1844)

--- codebase/packages/server/newrelic.js
+++ codebase/packages/server/newrelic.js
@@ -42,12 +42,10 @@ exports.config = {
       'request.headers.authorization',
       'request.headers.proxyAuthorization',
       'request.headers.setCookie*',
-      'request.headers.x*',
       'response.headers.cookie',
       'response.headers.authorization',
       'response.headers.proxyAuthorization',
       'response.headers.setCookie*',
-      'response.headers.x*'
     ]
   }
 }
