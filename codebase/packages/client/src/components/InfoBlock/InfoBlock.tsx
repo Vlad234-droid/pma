3e546243b1d28959b59dea40505414d2749a4f26
Mateusz Chodyna <Mateusz.Chodyna@tesco.com>
2023-04-04 12:51:49 +0200

    fix(client): PMA-3730 change links UI

--- codebase/packages/client/src/components/InfoBlock/InfoBlock.tsx
+++ codebase/packages/client/src/components/InfoBlock/InfoBlock.tsx
@@ -8,16 +8,28 @@ const InfoBlock: FC<{ text: string }> = ({ text }) => {
 
   return (
     <div className={css(wrapperStyle)} data-test-id={WRAPPER_ID}>
-      <Icon graphic='information' size={'18px'} />
+      <Icon graphic='information' size={'20px'} />
       <span className={css(textStyle)}>{text}</span>
     </div>
   );
 };
 
-const wrapperStyle: Rule = { display: 'flex', alignItems: 'center' };
+const wrapperStyle: Rule = ({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+
+  ':hover': {
+    textDecoration: 'underline',
+    color: theme.colors['hoverBlue'],
+
+    '& path': {
+      fill: theme.colors['hoverBlue'],
+    },
+  },
+});
 
 const textStyle: Rule = ({ theme }) => ({
-  ...theme.font.fixed.f14,
+  ...theme.font.fixed.f16,
   color: theme.colors.tescoBlue,
   padding: `${theme.spacing.s0} ${theme.spacing.s2}`,
 });
